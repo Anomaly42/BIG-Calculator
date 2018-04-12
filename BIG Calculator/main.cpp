ea821a601fc8310428d928911eee49f8d872e8d6
Anomaly42 <38323471+Anomaly42@users.noreply.github.com>
2018-04-12 20:58:24 +0530

    Update main.cpp

--- BIG Calculator/main.cpp
+++ BIG Calculator/main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
+	
 	Number num1;
-	num1.number = "675.978687"; // Don't forget a ".0" after an integer!
-	num1.sign = 1;
-	// if 0 then number is positive , if 1 then number is negative
-	num1.process();
+	num1.number = "675.978687"; 	// Don't forget a ".0" after an integer!
+	num1.sign = 1;			// if 0 then number is positive , if 1 then number is negative
+					
+	num1.process();			//Does all the necessary conversions to store the number in array format
 
 	Number num2;
 	num2.number = "777.007";
@@ -16,9 +17,9 @@ int main() {
 
 	Number num3;
 
-	num3 = num1 + num2;
+	num3 = num1 + num2;		// '+' Operator will add two numbers
 
-	num1.show() ;
+	num1.show() ;			// displays number in cout
 	cout << endl << "+" << endl;
 
 	num2.show();
