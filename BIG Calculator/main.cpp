3bdd481c9d04759abd1caf1b78d0a0e3d6c61ed2
Anomaly42 <38323471+Anomaly42@users.noreply.github.com>
2018-04-13 14:18:06 +0530

    Update main.cpp

--- BIG Calculator/main.cpp
+++ BIG Calculator/main.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 int main() {
-	
 	Number num1;
-	num1.number = "675.978687"; 	// Don't forget a ".0" after an integer!
-	num1.sign = 1;			// if 0 then number is positive , if 1 then number is negative
-					
-	num1.process();			//Does all the necessary conversions to store the number in array format
+	num1.number = "58858587696960.9697669696"; // Don't forget a ".0" after an integer!
+	num1.sign = 0;
+	// if 0 then number is positive , if 1 then number is negative
+	num1.process();
 
 	Number num2;
-	num2.number = "777.007";
+	num2.number = "86786796969769.9879869698";
 	num2.sign = 0;
 	num2.process();
 
 	Number num3;
 
-	num3 = num1 + num2;		// '+' Operator will add two numbers
+	num3 = num1 * num2;
 
-	num1.show() ;			// displays number in cout
-	cout << endl << "+" << endl;
+	num1.show() ;
+	cout << endl << "*" << endl;
 
 	num2.show();
 	cout << endl << "=" << endl;
